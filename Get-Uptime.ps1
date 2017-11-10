aae8030afca0f7a44c248f859059e53a2fc75d2c
Mike McGlothern-ADMIN <mikem-adm@woodstone.net>
2017-11-10 08:19:57 -0800

    .

---
+++ Get-Uptime.ps1
@@ -0,0 +1,33 @@
+#Function to retrieve uptime in parseable format. 
+#Get-WmiObject is used instead of Get-CimInstance to provide backward compatibility with POSH 1.0 and 2.0.
+#Source logic: https://blogs.technet.microsoft.com/heyscriptingguy/2013/03/27/powertip-get-the-last-boot-time-with-powershell/
+#Written by Mike McGlothern 11/9/2017, version 1.
+
+function Get-Uptime{
+    <#
+    .SYNOPSIS
+    Function to retrieve uptime in parseable format. 
+    .DESCRIPTION
+    Function uses WMI to retrieve uptime.  Uptime is returned in readable format (days/hours/minutes/seconds).
+    .EXAMPLE
+    Return uptime from a text list of computer names
+    get-content c:\ComputerList.txt | get-uptime
+    .PARAMETER computername
+    The computer name to query. Pipeline input is accepted.
+    .NOTES
+    You need to run this function as a member of the Domain Admins group; doing so is the only way to ensure you have permission to query WMI from the remote computers.
+    #>
+  
+      Param(
+          [Parameter(ValueFromPipeline)]
+          $ComputerName = $env:COMPUTERNAME
+      )
+      process {
+          if ($c=Get-WmiObject win32_operatingsystem -ComputerName $ComputerName){
+              [datetime]::Now - $c.ConverttoDateTime($c.lastbootuptime)
+          }
+          else {
+              Write-Error "Unable to retrieve WMI Object win32_operatingsystem from $ComputerName"
+          } 
+      }
+  }
